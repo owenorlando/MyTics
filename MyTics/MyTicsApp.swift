ce46731263f16c27683a4380f4ee21a2270b45be
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2024-05-17 10:07:30 +0700

    Initial Commit

---
+++ MyTics/MyTicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTicsApp.swift
+//  MyTics
+//
+//  Created by MacBook Pro on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
